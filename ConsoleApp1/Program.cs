bcf15099bc89710434a93ff2f8023e22f6a4d835
Yura <Yura@KHIRFA>
2022-03-26 18:12:56 +0700

    Add project files.

---
+++ ConsoleApp1/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //membuat object taxi
+            Taxi taxi = new Taxi();
+
+            //pengesetan nilai properties
+            taxi.DriveName = "Jono";
+            taxi.OnDuty = "Yes";
+            taxi.NumPassenger = 10;
+
+            //pemanggilan method
+            taxi.TaxiInfo();
+            taxi.PickUpPassenger();
+            taxi.DropOffPassenger();
+
+            Console.ReadKey();
+        }
+    }
+}
---
+++ ConsoleApp1/Taxi.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+    class Taxi
+    {
+        //properties
+        public string DriveName { get; set; }
+        public string OnDuty { get; set; }
+        public int NumPassenger { get; set; }
+
+        //method
+        public void TaxiInfo()
+        {
+            Console.WriteLine("Drive Name: {0}", DriveName);
+            Console.WriteLine("On Duty: {0}", OnDuty);
+            Console.WriteLine("Number of Passenger: {0}", NumPassenger);
+        }
+        
+        public void PickUpPassenger()=>
+            Console.WriteLine("{0} Sedang menjemput penumpang", DriveName);
+
+        public void DropOffPassenger()=>
+            Console.WriteLine("{0} Selesai mengantar penumpang", DriveName);
+    }
+}
